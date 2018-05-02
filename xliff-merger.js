780d691e493e0bf3b9d9e45185b4a6df2568e523
Evan MacTaggart <mactagge@fcc.ca>
2018-05-02 15:44:58 -0600

    Marginally better interface.

--- xliff-merger.js
+++ xliff-merger.js
@@ -1,17 +1,33 @@
+var path = require('path');
 var xslt4node = require('xslt4node');
 
+var sourceFile = process.argv[2] ? path.resolve(process.argv[2]) : "";
+var resultFile = process.argv[3] ? path.resolve(process.argv[3]) : "";
+var xsltFile = process.argv[4] ? path.resolve(process.argv[4]) : "";
+
+if (!sourceFile) {
+  throw new Error('Error: Please provide source path.');
+}
+
+if (!resultFile) {
+  throw new Error('Error: Please provide output path.');
+}
+
+if (!xsltFile) {
+  throw new Error('Error: Please provide template path.');
+}
+
 var config = {
-    xsltPath: 'xliff-merger.template.xsl',
-    sourcePath: 'base.xlf',
-    result: 'merged.xlf'
+  sourcePath: sourceFile,
+  result: resultFile,
+  xsltPath: xsltFile
 };
 
-console.log("Transforming", config.sourcePath, "to", config.result);
+console.log("-- XLIFF-MERGER: Transforming 🤖 --", config.sourcePath, "to", config.result);
 
 xslt4node.transform(config, function (err) {
-    if (err) {
-        console.log(err);
-    } else
-        console.log("Done!");
+  if (err) {
+    console.log(err);
+  } else
+    console.log("Done!");
 });
-
